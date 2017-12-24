633e1c43a2a6669d3f4e64101c1c53420b91d648
Xavier Harris <xharris1@umbc.edu>
2017-12-24 11:05:22 -0500

    added close button

---
+++ src/js/page.js
@@ -0,0 +1,7 @@
+class Page {
+	constructor (width, height) {
+		el_edit_space = app.getElement("#edit-space");
+	}
+}
+
+module.exports = Page;
--- src/main.js
+++ src/main.js
@@ -1,6 +1,18 @@
 var nwGUI = require('nw.gui');
 var nwWIN = nwGUI.Window.get();
 
+var app = {
+	getElement: function(sel) {
+		return document.querySelector(sel);
+	},
+
+	close: function() {
+		nwWIN.close();
+	}
+}
+
+const Page = require('./js/page');
+
 nwWIN.on('loaded', function() {
 	nwWIN.showDevTools();
 });
